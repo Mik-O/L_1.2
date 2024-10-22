812a2916405b3754f77bc9409006dea0ea6c8c04
mikle <mikle.kontinent@gmail.com>
2024-10-22 15:11:42 +0700

    ui design

---
+++ L_1.2/L_1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L_1.2
+//
+//  Created by Таня Кожевникова on 22.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
